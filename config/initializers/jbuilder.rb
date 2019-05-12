180d113aaded995c617513d1b434eea11bb39fc9
Stephen Hogsten <stephenhogsten@gmail.com>
2019-05-12 15:15:45 -0500

    fix location of jbuilder init

---
+++ config/initializers/jbuilder.rb
@@ -0,0 +1,2 @@
+# convert json keys from snake_case to lowerCamelCase
+Jbuilder.key_format camelize: :lower
--- config/jbuilder.rb
+++
@@ -1,2 +0,0 @@
-# convert snake_case to lowerCamelCase
-Jbuilder.key_format camelize: :lower
